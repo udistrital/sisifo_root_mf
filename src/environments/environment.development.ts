22e03d7ad6dc1aa3edde104cfe0f4c6994ccd65c
edwargl7 <07edwargl@gmail.com>
2024-09-13 07:08:07 -0500

    feat: actualizada url mf

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -12,16 +12,16 @@ export const environment = {
     CLIENTE_ID: "1OaC8gIVEfG8WxjrB37saszNQ5ga",
     RESPONSE_TYPE: "id_token token",
     SCOPE: "openid email role documento",
-    REDIRECT_URL: "https://pruebasisifomicroclientes.portaloas.udistrital.edu.co",
+    REDIRECT_URL: "https://pruebassisifomicroclientes.portaloas.udistrital.edu.co",
     SIGN_OUT_URL:
       "https://autenticacion.portaloas.udistrital.edu.co/oidc/logout",
     SIGN_OUT_REDIRECT_URL:
-    "https://pruebasisifomicroclientes.portaloas.udistrital.edu.co",
+    "https://pruebassisifomicroclientes.portaloas.udistrital.edu.co",
     AUTENTICACION_MID:
       "https://autenticacion.portaloas.udistrital.edu.co/apioas/autenticacion_mid/v1/token/userRol",
   },
   parcels: {
-    "@udistrital/root-config": "https://pruebasisifomicroclientes.portaloas.udistrital.edu.co/udistrital-root-config.js",
+    "@udistrital/root-config": "https://pruebassisifomicroclientes.portaloas.udistrital.edu.co/udistrital-root-config.js",
     "@udistrital/core-mf": "https://pruebascoreclientes.portaloas.udistrital.edu.co/main.js",
     "@udistrital/sisifo-historico-roles-mf": "https://pruebashistoricoroles.portaloas.udistrital.edu.co/main.js",
     "@udistrital/plan-anual-auditoria-mf": "//localhost:4203/main.js"
