f5dc4997db2127008e9e9e374edbf11b65f3d0f5
Mauricio Alvarado <mauriciorianoa@gmail.com>
2024-11-28 10:02:22 -0500

    feat: Ajuste de Urls

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -12,15 +12,19 @@ export const environment = {
     CLIENTE_ID: "2KK6pV4LPkc93Z2ZHwxXCcA2yD4a",
     RESPONSE_TYPE: "id_token token",
     SCOPE: "openid email role documento",
-    REDIRECT_URL: "https://pruebassisifomicroclientes.portaloas.udistrital.edu.co",
+    REDIRECT_URL:
+      "https://pruebassisifomicroclientes.portaloas.udistrital.edu.co",
     SIGN_OUT_URL:
       "https://autenticacion.portaloas.udistrital.edu.co/oidc/logout",
-    SIGN_OUT_REDIRECT_URL: "https://pruebassisifomicroclientes.portaloas.udistrital.edu.co",
+    SIGN_OUT_REDIRECT_URL:
+      "https://pruebassisifomicroclientes.portaloas.udistrital.edu.co",
     AUTENTICACION_MID:
       "https://autenticacion.portaloas.udistrital.edu.co/apioas/autenticacion_mid/v1/token/userRol",
   },
   parcels: {
-    "@udistrital/root-config": "https://pruebassisifomicroclientes.portaloas.udistrital.edu.co/udistrital-root-config.js",
-    "@udistrital/core-mf": "https://pruebascoreclientes.portaloas.udistrital.edu.co/main.js"
+    "@udistrital/root-config":
+      "https://pruebassisifomicroclientes.portaloas.udistrital.edu.co/udistrital-root-config.js",
+    "@udistrital/core-mf":
+      "https://pruebascoreclientes.portaloas.udistrital.edu.co/main.js",
   },
 };
--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -13,13 +13,17 @@ export const environment = {
     RESPONSE_TYPE: "id_token token",
     SCOPE: "openid email role documento",
     REDIRECT_URL: "https://sisifomicroclientes.portaloas.udistrital.edu.co",
-    SIGN_OUT_URL: "https://autenticacion.portaloas.udistrital.edu.co/oidc/logout",
-    SIGN_OUT_REDIRECT_URL: "https://sisifomicroclientes.portaloas.udistrital.edu.co",
+    SIGN_OUT_URL:
+      "https://autenticacion.portaloas.udistrital.edu.co/oidc/logout",
+    SIGN_OUT_REDIRECT_URL:
+      "https://sisifomicroclientes.portaloas.udistrital.edu.co",
     AUTENTICACION_MID:
       "https://autenticacion.portaloas.udistrital.edu.co/apioas/autenticacion_mid/v1/token/userRol",
   },
   parcels: {
-    "@udistrital/root-config": "https://sisifomicroclientes.portaloas.udistrital.edu.co/udistrital-root-config.js",
-    "@udistrital/core-mf": "https://coreclientes.portaloas.udistrital.edu.co/main.js",
+    "@udistrital/root-config":
+      "https://sisifomicroclientes.portaloas.udistrital.edu.co/udistrital-root-config.js",
+    "@udistrital/core-mf":
+      "https://coreclientes.portaloas.udistrital.edu.co/main.js",
   },
 };
