94c006b6c73ee37a80bb4215def11b791c975664
Mauricio Alvarado <mauriciorianoa@gmail.com>
2024-11-28 09:55:12 -0500

    feat: Ajuste de Host
    https://github.com/udistrital/argo_documentacion/issues/232

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -20,7 +20,7 @@ export const environment = {
       "https://autenticacion.portaloas.udistrital.edu.co/apioas/autenticacion_mid/v1/token/userRol",
   },
   parcels: {
-    "@udistrital/root-config": "https://pruebasargomicroclientes.portaloas.udistrital.edu.co/udistrital-root-config.js",
+    "@udistrital/root-config": "https://pruebassisifomicroclientes.portaloas.udistrital.edu.co/udistrital-root-config.js",
     "@udistrital/core-mf": "https://pruebascoreclientes.portaloas.udistrital.edu.co/main.js"
   },
 };
